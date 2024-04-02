090232e75c799deda720171da3ac15257a91301e
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-04-02 11:49:23 +0800

    Included view list by day and new test class for timetable.

--- src/test/java/Timetable/TimetableTest.java
+++ src/test/java/Timetable/TimetableTest.java
@@ -1,13 +1,4 @@
 package Timetable;
-import expenditure.ExpenditureList;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimetableTest {
 
