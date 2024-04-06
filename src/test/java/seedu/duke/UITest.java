6116bf9cc0787442bd87eca68ffe56e4947d4016
Mmaxx15 <leongxwmax@gmail.com>
2024-04-06 12:52:56 +0800

    remove unused imports

--- src/test/java/seedu/duke/UITest.java
+++ src/test/java/seedu/duke/UITest.java
@@ -4,7 +4,6 @@ import CantvasUI.UI;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UITest {
@@ -13,7 +12,7 @@ public class UITest {
     void printHelpMessage() {
         ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStreamCaptor));
-        
+
         UI.printHelpMessage();
         String output = outputStreamCaptor.toString().trim();
         assertTrue(output.contains("Cantvas Help"));
