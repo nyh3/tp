e7ac8667fff48bf07196969ebeb513a9def872c5
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-04-10 05:15:15 +0800

    Fixed J-unit test cases to pass gradle check.

--- src/test/java/timetable/TimetableTest.java
+++ src/test/java/timetable/TimetableTest.java
@@ -94,7 +94,7 @@ public class TimetableTest {
         TimetableList.addClass("day/1 code/CDE2501 time/10 duration/2 location/Room 101", true);
         outContent.reset();
         TimetableList.addClass("day/1 code/CG2111A time/10 duration/1 location/Room 102", false);
-        assertTrue(outContent.toString().contains("There's already a class scheduled at this time."));
+        assertEquals("There's already a class scheduled during this time period.", outContent.toString().trim());
     }
 
     @Test
