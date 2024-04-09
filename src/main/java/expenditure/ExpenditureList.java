1cb6c19c915aab4bc2b7a6ad9748c6ab01ee23ef
Mmaxx15 <leongxwmax@gmail.com>
2024-04-10 01:12:00 +0800

    actually fixed variable access order

--- src/main/java/expenditure/ExpenditureList.java
+++ src/main/java/expenditure/ExpenditureList.java
@@ -11,13 +11,13 @@ import java.util.List;
 import java.text.DecimalFormat;
 
 public class ExpenditureList {
+    public static int expenditureCount;
     private static final double MINIMUM_AMOUNT = 0.00F;
     private static final float MAX_AMOUNT = 9999999.99F;
     private static final int MONTHYEARINPUTLENGTH = 7;
     private static final int MAX_DESCRIPTION_LENGTH = 100;
     private static final int MAX_TYPE_LENGTH = 20;
     private static ArrayList<Expenditure> expenditureList = new ArrayList<>();
-    public static int expenditureCount;
 
     public ExpenditureList() {
         expenditureList = new ArrayList<>();
