a0b490fb2da4aba3215c248c68c3ac58964accb2
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-04-13 22:38:34 +0800

    Javadoc for expenditure and class.

--- src/main/java/expenditure/Expenditure.java
+++ src/main/java/expenditure/Expenditure.java
@@ -8,6 +8,14 @@ public class Expenditure {
     protected Float amount;
     protected String date;
 
+    /**
+     * Constructor for Expenditure.
+     *
+     * @param description The description of the expenditure.
+     * @param type The type of the expenditure.
+     * @param amount The amount of the expenditure.
+     * @param date The date of the expenditure.
+     */
     public Expenditure(String description, String type, Float amount, String date) {
         this.description = description;
         this.type = type;
@@ -15,27 +23,57 @@ public class Expenditure {
         this.date = date;
     }
 
+    /**
+     * Retrieves the description of the expenditure.
+     *
+     * @return The description of the expenditure.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Retrieves the type of the expenditure.
+     *
+     * @return The type of the expenditure.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Retrieves the amount of the expenditure.
+     *
+     * @return The amount of the expenditure.
+     */
     public Float getAmount() {
         return amount;
     }
 
+    /**
+     * Retrieves the date of the expenditure.
+     *
+     * @return The date of the expenditure.
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Returns a string representation of the expenditure.
+     *
+     * @return A string representation of the expenditure.
+     */
     @Override
     public String toString() {
         return description + " | " + type + " | Cost: $" + amount + " | date: " + date;
     }
 
+    /**
+     * Returns a string representation of the expenditure for storage.
+     *
+     * @return A string representation of the expenditure for storage.
+     */
     public String toStringStorage() {
         return description + " | " + type + " | " + amount + " | " + date;
     }
