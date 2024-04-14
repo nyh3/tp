4077e5dc0949504623673cee4c650d0c8199fb61
Mmaxx15 <leongxwmax@gmail.com>
2024-04-14 17:37:02 +0800

    testings for processcommand

--- src/test/java/ProcessCommand/ProcessCommandTest.java
+++ src/test/java/ProcessCommand/ProcessCommandTest.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProcessCommandTest {
 
@@ -52,6 +53,78 @@ public class ProcessCommandTest {
         assertEquals(0, ExpenditureList.expenditureCount);
         assertEquals("Unknown command. Please try again! Type " +
                 "'help' for more information!", outContent.toString().trim());
+        outContent.reset();
+    }
+
+    @Test
+    public void testSpacesBetweenCommands() {
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        ExpenditureList expenditureList = new ExpenditureList();
+        ProcessCommand processCommand = new ProcessCommand();
+
+        // missing spaces 1
+        processCommand.userCommand("e/add/ d/ Lunch t/Food amt/10.12 date/02.02.2023", expenditureList);
+        assertEquals(0, ExpenditureList.expenditureCount);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!"
+                , outContent.toString().trim());
+        outContent.reset();
+
+        // missing spaces 2
+        processCommand.userCommand("e/add/d/ Lunch t/Food amt/10.12 date/02.02.2023", expenditureList);
+        assertEquals(0, ExpenditureList.expenditureCount);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!"
+                , outContent.toString().trim());
+        outContent.reset();
+
+        // missing spaces 3
+        processCommand.userCommand("e/add/d/Lunch t/Food amt/10.12 date/02.02.2023", expenditureList);
+        assertEquals(0, ExpenditureList.expenditureCount);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!"
+                , outContent.toString().trim());
+        outContent.reset();
+
+        // missing spaces 4
+        processCommand.userCommand("e/add/d/Luncht/Food amt/10.12 date/02.02.2023", expenditureList);
+        assertEquals(0, ExpenditureList.expenditureCount);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!", outContent.toString().trim());
+        outContent.reset();
+
+        // missing spaces 5
+        processCommand.userCommand("e/add/d/Luncht/Foodamt/10.12 date/02.02.2023", expenditureList);
+        assertEquals(0, ExpenditureList.expenditureCount);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!", outContent.toString().trim());
+        outContent.reset();
+
+    }
+
+    @Test
+    public void invalidCommand() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        ProcessCommand processCommand = new ProcessCommand();
+        ExpenditureList expenditureList = new ExpenditureList();
+
+        processCommand.userCommand(" ", expenditureList);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!", outContent.toString().trim());
+        outContent.reset();
+
+        processCommand.userCommand("1111111111111", expenditureList);
+        assertEquals("Unknown command. Please try again! Type 'help' for more information!", outContent.toString().trim());
+        outContent.reset();
+
+    }
+
+    @Test
+    public void helpCommand() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        ProcessCommand processCommand = new ProcessCommand();
+        ExpenditureList expenditureList = new ExpenditureList();
+
+        processCommand.userCommand("help", expenditureList);
+        assertTrue(outContent.toString().contains("Cantvas Help"));
         outContent.reset();
     }
 
