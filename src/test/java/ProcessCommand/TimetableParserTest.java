2329406b89ff0193a66495f1b33da194ec756de9
Mmaxx15 <leongxwmax@gmail.com>
2024-04-14 13:37:13 +0800

    Combined Haotians test with mine

--- src/test/java/ProcessCommand/TimetableParserTest.java
+++ src/test/java/ProcessCommand/TimetableParserTest.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import processusercommands.TimetableParser;
 import timetable.TimetableList;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TimetableParserTest {
 
@@ -25,6 +24,48 @@ public class TimetableParserTest {
     @AfterEach
     public void tearDown() {
         System.setOut(originalOut);
+    }
+
+    @Test
+    void testParseTimetableAdd() {
+        TimetableParser.parseTimetable("tt/ add/description here");
+        assertFalse(outContent.toString().contains("Adding class logic"), "Output should indicate that " +
+                "adding class logic was called");
+    }
+
+    @Test
+    void testParseTimetableDelete() {
+        TimetableParser.parseTimetable("tt/ del/description here");
+        assertFalse(outContent.toString().contains("Deleting class logic"), "Output should " +
+                "indicate that deleting class logic was called");
+    }
+
+    @Test
+    void testParseTimetableList() {
+        TimetableParser.parseTimetable("tt/ list");
+        assertFalse(outContent.toString().contains("Listing timetable"), "Output should indicate that " +
+                "listing timetable logic was called");
+    }
+
+    @Test
+    void testParseTimetableListByDay() {
+        TimetableParser.parseTimetable("tt/ list -d/ Monday");
+        assertFalse(outContent.toString().contains("Listing by day"), "Output should indicate that " +
+                "listing by day logic was called");
+    }
+
+    @Test
+    void testParseTimetableUnknownCommand() {
+        TimetableParser.parseTimetable("tt/ foo/description here");
+        assertTrue(outContent.toString().contains("Unknown command description here"), "Output " +
+                "should indicate unknown command was encountered");
+    }
+
+    @Test
+    void testParseTimetableIncompleteCommand() {
+        TimetableParser.parseTimetable("tt/ add");
+        assertTrue(outContent.toString().contains("Incomplete command!"), "Output should indicate " +
+                "incomplete command was encountered");
     }
 
     @Test
